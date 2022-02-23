ba6e06e809b39d299dd2e7d4d828704c30063782
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-23 10:35:18 +0700

    first commit

---
+++ app/Traits/Nanoids.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+use Hidehalo\Nanoid\Client;
+
+trait Uuids
+{
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $client = new Client();
+                $model->{$model->getKeyName()} = $client->formattedId($alphabet = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', $size = 21);
+            }
+        });
+    }
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
