cbcac972430b6ee9cde8f391938d8eface2541d6
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-23 15:23:34 +0700

    new helper center

---
+++ app/Http/Controllers/HelpCenterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HelpCenter;
+use Illuminate\Http\Request;
+
+class HelpCenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HelpCenter  $helpCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HelpCenter $helpCenter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HelpCenter  $helpCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HelpCenter $helpCenter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HelpCenter  $helpCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HelpCenter $helpCenter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HelpCenter  $helpCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HelpCenter $helpCenter)
+    {
+        //
+    }
+}
